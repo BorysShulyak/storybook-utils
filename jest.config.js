e283a7069a96c7b79b4118b1f81d51eb2fe661f0
boris.shulyak <borysshulyak@gmail.com>
2023-08-14 15:05:25 +0300

    feat(repository): set up

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+const jestConfig = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,cjs}', '!**/node_modules/**', '!**/constants/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0
+    }
+  },
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      { classNameTemplate: '{classname}', titleTemplate: '{title}', ancestorSeparator: ' > ' }
+    ]
+  ],
+  moduleFileExtensions: ['js', 'cjs'],
+  rootDir: './',
+  roots: ['<rootDir>/src/'],
+  verbose: true,
+  transform: {
+    '\\.[jt]s$': 'babel-jest'
+  }
+};
+
+module.exports = jestConfig;
